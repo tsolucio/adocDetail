b6d70a2bf074ce2df6607ca38cfb8bb34f0cce5e
Joe Bordes <joe@tsolucio.com>
2014-09-28 19:36:21 +0200

    fix language files

--- modules/Adocdetail/language/en_us.lang.php
+++ modules/Adocdetail/language/en_us.lang.php
@@ -1,13 +1,5 @@
 <?php
-/*+**********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- ************************************************************************************/
- /*************************************************************************************************
+/*************************************************************************************************
  * Copyright 2014 Opencubed -- This file is a part of TSOLUCIO coreBOS customizations.
  * You can copy, adapt and distribute the work under the "Attribution-NonCommercial-ShareAlike"
  * Vizsage Public License (the "License"). You may not use this file except in compliance with the
@@ -20,20 +12,19 @@
  * See the License for the specific language governing permissions and limitations under the
  * License terms of Creative Commons Attribution-NonCommercial-ShareAlike 3.0 (the License).
  *************************************************************************************************
- *  Module       : Adecuaciones
+ *  Module       : Adocdetail
  *  Version      : 5.4.0
  *  Author       : Opencubed
  *************************************************************************************************/
 
 $mod_strings = Array(
-	'ModuleName' => 'Module Name',
-	'SINGLE_ModuleName' => 'Module Name',
-	'ModuleName ID' => 'Module Name ID',
+	'ModuleName' => 'Adoc detail',
+	'SINGLE_ModuleName' => 'Adoc detail',
+	'ModuleName ID' => 'Adoc detail ID',
 
 	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
-	'LBL_MODULEBLOCK_INFORMATION' => 'ModuleBlock Information',
+	'LBL_MODULEBLOCK_INFORMATION' => 'Adoc detail Information',
 
-	'ModuleFieldLabel' => 'ModuleFieldLabel Text',
 );
 
 ?>
